ba4de5f96d3aec475fcbc410c643cde2e1257a13
Ahmadissa23 <94993475+Ahmadissa23@users.noreply.github.com>
2022-06-05 16:55:39 -0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,20 +1,18 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "abuissa200096@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['Your Name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Subject";
-    $subject2 = "Copy of your form Subject";
-    $message = $name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+
+$name = $_POST['Your Name'];
+$subject = $_POST['Subject'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
+
+$mailTo = "abuissa200096@gmail.com";
+$headers = "From: ".$mailFrom
+
+$txt = "you have received an email from ".$name".\n\n".$message;
+mail($mailTo,$subject,$txt,$headers);
+header("location: index.html?mailsend");
+
+  echo $contact->send();
 ?>
