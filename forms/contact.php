15408729f21049edcbad8f893022cafbe8acb078
Ahmadissa23 <94993475+Ahmadissa23@users.noreply.github.com>
2022-11-21 12:04:38 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -10,7 +10,7 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(name,email,subject,message) values(?, ?, ?, ?)");
+		$stmt = $conn->prepare("insert into form(name,email,subject,message) values(?, ?, ?, ?)");
 		$stmt->bind_param("ssss", $name,$email, $subject, $message);
 		$execval = $stmt->execute();
 		echo $execval;
